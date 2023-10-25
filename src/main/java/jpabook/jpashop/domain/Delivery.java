fbf419e375039de21c1aaf09f20cd3a4e76ca04f
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-25 17:11:03 +0900

    feat: Entity class 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    // EnumType은 반드시 STRING으로 사용! ORDINAL은 숫자로 부여되므로 중간에 타입이 추가될 경우 하나씩 밀려 처음 부여된 숫자와 달라져 DB에서 조회가 안됨(XXX로 나옴)
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
+
+}
