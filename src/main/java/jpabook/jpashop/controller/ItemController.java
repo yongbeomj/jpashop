9f59a9883a0b444edd442ac294b5cd693de2eda3
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-28 10:15:41 +0900

    feat: 상품 등록 view 개발
    상품은 Book만 진행

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    // setter를 안쓰는 코드가 좋은 설계
+    // 수정이 필요할 경우 메서드를 만들고 호출하는 방식으로 사용하는게 더 좋다
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
