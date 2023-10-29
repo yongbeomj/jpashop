2bf805536c2c8c3fbdbc45e2a593cb6b2abdd69c
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-30 02:35:45 +0900

    feat: 상품 수정 view 개발
    변경 감지/병합 강의 - 준영속 엔티티

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,29 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // 변경 감지 기능 사용 (권장)
+    // merge 사용할 경우 모든 값을 변경 (기존 값이 유지 되는게 아님. 즉, 값이 없는 경우 null로 업데이트)
+    // 실무에서는 제약 조건이 많으므로 merge보다는 변경 감지를 사용하여 업데이트
+//    @Transactional
+//    public void updateItem(Long itemId, Book param) {
+//        Item findItem = itemRepository.findOne(itemId);
+//        // setter를 사용하지 말고 메서드를 사용하는 것을 권장 (다시 한 번 강조!)
+//        findItem.setPrice(param.getPrice());
+//        findItem.setName(param.getName());
+//        findItem.setStockQuantity(param.getStockQuantity());
+//    }
+
+    // parameter가 많을 경우 dto를 생성하여 전달
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        // setter를 사용하지 말고 메서드를 사용하는 것을 권장 (다시 한 번 강조!)
+        // findItem.change(name, price, stockQuantity);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
