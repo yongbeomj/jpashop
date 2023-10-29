e9a460f282f749ce90510c1bf038a4a6adf45704
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-30 04:09:11 +0900

    feat: 회원등록 api 개발 (v1)
    문제점 - 엔티티를 requestbody에 직접 매핑
    실무에서 하나의 엔티티에 각각의 api를 위한 모든 요구사항을 담기는 어려움
    엔티티가 변경되면 api 스펙(프로퍼티 등)이 변함 > 가장 큰 문제

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController // @Controller + @ResponseBody
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    // 회원등록
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
