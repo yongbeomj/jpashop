ff6c45fc111d6d9f37a9aa2d021450659f3223d6
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-31 14:25:04 +0900

    feat: 주문조회 api (v1)

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 양방향 연관관계 무한루프를 끊기 위해 한 쪽에 @JsonIgnore 추가 필요
+    // 엔티티를 직접 노출하므로 좋은 방법이 아님
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
