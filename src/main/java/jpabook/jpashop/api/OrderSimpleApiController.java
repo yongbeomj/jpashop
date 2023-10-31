4931d27a07821468f6085174692a24fd101fee1b
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-31 14:02:54 +0900

    refactor: 주문조회 api 성능 최적화 (v4)
    dto에서 원하는 값만 조회

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -51,6 +54,16 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    // v3와 v4의 차이는 조회하는 데이터 변수 개수 차이
+    // v4는 필요한 값만 조회하므로 v3보다 최적화되어있지만 성능상으로 별 차이 없음 (아주 미세함)
+    // 보통 성능 이슈는 select 절이 아니라 join할 때, index 잘못 잡혔을 때 등 발생
+    // 그리고 v4는 api 스펙에 맞춰져 있으므로 유연성이 없음. (수정 필요 시 repository, dto 모두 수정해야 함)
+    // 실시간으로 트래픽이 많은 api의 경우는 최적화 고민해 볼 필요는 있음
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
